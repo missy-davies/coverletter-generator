71e8af3c71a77a07d8500d09cb2f0960bed679de
Missy Davies <ms.melissadavies@gmail.com>
2021-03-12 09:46:53 +0000

    Started view function for home and start pages and wrote corresponding draft of html

--- coverletter-generator.py
+++ coverletter-generator.py
@@ -1,3 +1,33 @@
 """A cover letter generator"""
 
 from flask import Flask, render_template, request 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    """A landing page with a call to action"""
+
+    return render_template("home-page.html")
+
+
+@app.route('/start')
+def get_name():
+    """Basic instructions and get user's name"""
+
+    return render_template("start.html")
+
+# @app.route('/job')
+
+
+
+
+# @app.route('/skills')
+
+
+
+# @app.route('/experience')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
