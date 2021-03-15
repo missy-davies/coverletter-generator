485d9c9e9f83633a3f2b6e9999f95b9c42caecc7
Missy Davies <ms.melissadavies@gmail.com>
2021-03-15 07:20:50 +0000

    Add variables to session and insert into Jinja templates

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 """A cover letter generator"""
 
-
-from flask import Flask, redirect, request, render_template, session
-from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, request, render_template, redirect, session
+import jinja2
 
 
 app = Flask(__name__)
@@ -29,33 +28,54 @@ def get_job():
 
     full_name = request.args.get("full-name")
     name_list = full_name.split(" ")
-    first = name_list[0].title()
-    last = name_list[1].title() # might be extra and can remove 
 
-    return render_template("job.html", first=first)
+    first = name_list[0].title()
+    session['first'] = first
+
+    last = name_list[1].title() 
+    session['last'] = last
+
+    return render_template("job.html")
 
 
 @app.route('/skills')
 def get_skills():
     """Get information about skills"""
 
-    position = request.args.get("job-title")
+    job_title = request.args.get("job-title").title()
+    session['job-title'] = job_title
 
-    return render_template("skills.html", position=position)
+    company = request.args.get("company").title()
+    session['company'] = company
+
+    return render_template("skills.html")
 
 
 @app.route('/experience')
 def get_experience():
     """Get information about experience"""
 
-    company = request.args.get("company")
+    skill1 = request.args.get("skill-1").lower()
+    session['skill1'] = skill1
 
-    return render_template("experience.html", company=company)
+    skill2 = request.args.get("skill-2").lower()
+    session['skill2'] = skill2
+
+    skill3 = request.args.get("skill-3").lower()
+    session['skill3'] = skill3
+
+    return render_template("experience.html")
 
 
 @app.route('/education')
 def get_education():
     """Get information about education"""
+
+    years_experience = request.args.get("years-experience")
+    session['years_experience'] = years_experience
+
+    accomplishment = request.args.get("accomplishment")
+    session['accomplishment'] = accomplishment
 
     return render_template("education.html")
 
@@ -64,13 +84,15 @@ def get_education():
 def generate_coverletter():
     """Generate custom cover letter"""
 
+    edu = request.args.get("level-education")
+    session['level-education'] = edu
+
+    special = request.args.get("special")
+    session['special'] = special
+
     return render_template("cover-letter.html")
 
 
 
-if __name__ == '__main__':
-    app.debug = True
-
-    DebugToolbarExtension(app)
-
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
